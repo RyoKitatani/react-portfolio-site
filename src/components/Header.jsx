ea90832b60212c3a332306df195bfbd2d2b5dcd4
RyoKitatani <ryo0929m@gmail.com>
2022-01-24 22:03:05 +0900

    finished_4-3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
+
 export const Header = () => {
-  const componentName = () => 'header'
   return (
-    <div className="test-wrapper">
-     このテストは{componentName()}
-    </div>
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+      {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            <h1 className="title-text">ちゃたん</h1>
+            <h3 className="title-text">Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
